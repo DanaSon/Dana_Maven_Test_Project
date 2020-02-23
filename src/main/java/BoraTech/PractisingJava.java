b6f32db3024a052cebf090a11edd23f1fa49cc37
DanaSon <danason246@gmail.com>
2020-02-23 13:10:42 -0500

    added hew message to my first branch

--- src/main/java/BoraTech/PractisingJava.java
+++ src/main/java/BoraTech/PractisingJava.java
@@ -4,6 +4,9 @@ public class PractisingJava {
 
 	public static void main(String[] args) {
 		System.out.println("February 23, Sunday group study");
+		System.out.println("February 23, Sunday group study111111111111ggffhfghfgh");
+		
+		
 	}
 
 }
