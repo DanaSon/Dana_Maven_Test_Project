b2d69803f1157570b170f9df912a9d4e06646c2f
DanaSon <danason246@gmail.com>
2020-02-23 12:31:45 -0500

    Today was second practising

--- src/main/java/BoraTech/PractisingJava.java
+++ src/main/java/BoraTech/PractisingJava.java
@@ -4,6 +4,9 @@ public class PractisingJava {
 
 	public static void main(String[] args) {
 		System.out.println("February 23, Sunday group study");
+		
+		System.out.println("Today we are three people in group study");
+		
 	}
 
 }
